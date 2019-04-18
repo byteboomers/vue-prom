f2422fab74d1443ad700a3565520d996ebe47aeb
Bjorn <krols.bj@gmail.com>
2019-04-18 09:54:27 +0200

    Move to modern Babel setup

--- src/VueProm.vue
+++ src/VueProm.vue
@@ -1,28 +1,28 @@
 <template>
-    <span>
-      <!-- Pending -->
-        <div v-if="pending">
-            <slot v-if="$slots['pending']" name="pending"></slot>
-            <span v-else>Loading...</span>
-        </div>
-        <!-- Catch -->
-        <div v-else-if="rejected">
-            <slot v-if="$scopedSlots['catch']" name="catch" :error="error"></slot>
-            <slot v-else-if="$slots['catch']" name="catch"></slot>
-            <span v-else>Error</span>
-        </div>
-        <!-- Then -->
-        <div v-else>
-            <slot v-if="$scopedSlots['then']" name="then" :result="result"></slot>
-            <slot v-else-if="$slots['then']" name="then"></slot>
-            <span v-else>Loaded</span>
-        </div>
-    </span>
+  <span>
+    <!-- Pending -->
+    <div v-if="pending">
+      <slot v-if="$slots['pending']" name="pending"></slot>
+      <span v-else>Loading...</span>
+    </div>
+    <!-- Catch -->
+    <div v-else-if="rejected">
+      <slot v-if="$scopedSlots['catch']" name="catch" :error="error"></slot>
+      <slot v-else-if="$slots['catch']" name="catch"></slot>
+      <span v-else>Error</span>
+    </div>
+    <!-- Then -->
+    <div v-else>
+      <slot v-if="$scopedSlots['then']" name="then" :result="result"></slot>
+      <slot v-else-if="$slots['then']" name="then"></slot>
+      <span v-else>Loaded</span>
+    </div>
+  </span>
 </template>
 
 <script>
 export default {
-  props: ['promise', 'refresh'],
+  props: ["promise", "refresh"],
   created() {
     this.executePromise();
   },
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,27 @@
-const webpack = require('webpack');
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: path.resolve(__dirname + '/src/VueProm.vue'),
-    output: {
-        path: path.resolve(__dirname + '/dist/'),
-        filename: 'vue-prom.js',
-        library: 'vue-prom',
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            }
-        ]
-    }
+  entry: path.resolve(__dirname + "/src/VueProm.vue"),
+  output: {
+    path: path.resolve(__dirname + "/dist/"),
+    filename: "vue-prom.js",
+    library: "vue-prom",
+    libraryTarget: "umd",
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader"
+      }
+    ]
+  }
 };
